df7bc97bc1257a05008f355f4f0a3a3488a9bfff
nneeqq <bleadi426@gmail.com>
2021-04-28 14:09:38 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace lab4TS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            driver.FindElement(By.Name("q")).SendKeys("Computer");
+            driver.FindElement(By.Name("q")).SendKeys(Keys.Enter);
+            bool isPresent = driver.PageSource.Contains("head");
+            if (isPresent)
+            {
+                Console.WriteLine("TEST PASSED");
+            }
+            else
+            {
+                Console.WriteLine("TEST FAILED");
+            }
+            driver.Close();
+        }
+    }
+}
